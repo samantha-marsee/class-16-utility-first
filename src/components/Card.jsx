8779bbf499b1f358af0c61ae2635b944bb43b994
samantha-marsee <sam572@cornell.edu>
2025-03-19 15:24:45 +0000

    looks a little weird for some reason

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card({ imgUri, title, children }) {
     <div>
       <img src={imgUri} alt="" />
 
-      <div className = "rounded-xl bg-slate-200 max-w-2xs">
+      <div className = "rounded-xl bg-slate-200 max-w-2xs p-4">
         <h3>{title}</h3>
         <p>
           {children}
